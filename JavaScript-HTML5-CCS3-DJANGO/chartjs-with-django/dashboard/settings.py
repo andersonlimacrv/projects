7650978c72875930d8a5b986f240b126b9daf4f7
ANDERSON CARVALHO <andersonlimacrv@gmail.com>
2023-02-03 20:28:02 -0300

    Criado página base, e editado o modelo MVC - model, view e template, além de adicionado pasta STATIC para arquivos JS e CSS

--- JavaScript-HTML5-CCS3-DJANGO/chartjs-with-django/dashboard/settings.py
+++ JavaScript-HTML5-CCS3-DJANGO/chartjs-with-django/dashboard/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphics'
 ]
 
 MIDDLEWARE = [
@@ -110,13 +111,17 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT = os.path.join('static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
